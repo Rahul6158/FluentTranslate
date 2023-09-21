977476f6a31f4b7e52e9c23213d0b6b8c93b13c7
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-22 04:46:42 +0530

    Update page1.py

--- page1.py
+++ page1.py
@@ -145,7 +145,7 @@ def count_words(text):
     return len(words)
 
 def main():
-    st.image("jangirii.png", width=700)
+    st.image("jangirii.png", width=200)
     st.title("Text Translation and Conversion to Speech (English - other languages)")
 
     # Add a file uploader for DOCX, PDF, images, and TXT
