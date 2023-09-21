eb1afb87f120eb67a1ea5b40b77f3585003f133b
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-22 03:56:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,8 @@ import streamlit as st
 import page1
 import page2
 
-# Create a sidebar navigation using radio buttons with increased size
-page_choice = st.sidebar.radio("", ["Page 1", "Page 2"], key="page")
-
-# Add custom CSS styling to increase the size of sidebar elements
-st.markdown(
-    """
-    <style>
-    .css-17a69v6 {
-        font-size: 24px !important;
-    }
-    .st-e5 {
-        padding: 10px !important;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+# Create a sidebar navigation using radio buttons
+page_choice = st.sidebar.radio("Select a Page", ["Page 1", "Page 2"])
 
 if page_choice == "Page 1":
     page1.main()  # Call the main function for Page 1
