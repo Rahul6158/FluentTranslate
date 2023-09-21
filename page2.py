334e3d1c0654b01261fc99c3328e24a87d7c9f89
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-22 04:45:57 +0530

    Update page2.py

--- page2.py
+++ page2.py
@@ -67,14 +67,15 @@ language_mapping = {
     "xh": "Xhosa"
 }
 
-# Function to translate text using Google Translate
-def translate_text(text, target_language):
-    translator = Translator()
+# Function to translate text using Google Translate with error handling
+def translate_text_with_fallback(text, target_language):
     try:
+        translator = Translator()
         translated_text = translator.translate(text, dest=target_language)
         return translated_text.text
-    except ValueError:
-        return "Error: Target language not found in the mapping."
+    except Exception as e:
+        st.warning(f"Error during translation: {str(e)}")
+        return None
 
 # Function to convert text to speech and save as an MP3 file
 def convert_text_to_speech(text, output_file, language='en'):
@@ -91,6 +92,12 @@ def get_binary_file_downloader_html(link_text, file_path, file_format):
     download_link = f'<a href="data:{file_format};base64,{b64_file}" download="{os.path.basename(file_path)}">{link_text}</a>'
     return download_link
 
+# Function to convert text to a DOCX document
+def convert_text_to_word_doc(text, output_file):
+    doc = Document()
+    doc.add_paragraph(text)
+    doc.save(output_file)
+
 def main():
     st.image("jangirii.png", width=50)
     st.title("Text Translation and Conversion to Speech (English - other languages)")
@@ -99,32 +106,47 @@ def main():
     text = st.text_area("Enter text to translate and convert to speech:")
     target_language = st.selectbox("Select target language:", list(language_mapping.values()))
 
-    # Check if the target language is in the mapping
-    if st.button("Translate"):
-        for code, lang_name in language_mapping.items():
-            if target_language == lang_name:
-                translated_text = translate_text(text, code)
-                break
-        else:
-            st.warning("Target language not found in the mapping.")
+    # Add a button to trigger the translation and text-to-speech conversion
+    if st.button("Translate - Convert to Speech and get Translated document"):
+        # Define target_language_code within this scope
+        target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
+
+        # Translate text using Google Translate with error handling
+        translated_text = translate_text_with_fallback(text, target_language_code)
 
         # Display translated text
         if translated_text:
             st.subheader(f"Translated text ({target_language}):")
             st.write(translated_text)
-            
-            # Convert the translated text to speech and provide download links
-            if st.button("Convert to Speech"):
-                output_file = "translated_speech.mp3"
-                convert_text_to_speech(translated_text, output_file, language=target_language)
+        else:
+            st.warning("Translation result is empty. Please check your input text.")
 
-                # Play the generated speech
-                audio_file = open(output_file, 'rb')
-                st.audio(audio_file.read(), format='audio/mp3')
+        if translated_text:
+            # Convert translated text to speech
+            output_file = "translated_speech.mp3"
+            convert_text_to_speech(translated_text, output_file, language=target_language_code)
 
-                # Provide download link for the MP3 file
-                audio_download_link = get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3')
-                st.markdown(audio_download_link, unsafe_allow_html=True)
+            # Play the generated speech
+            audio_file = open(output_file, 'rb')
+            st.audio(audio_file.read(), format='audio/mp3')
+
+            # Play the generated speech (platform-dependent)
+            if os.name == 'posix':  # For Unix/Linux
+                os.system(f"xdg-open {output_file}")
+            elif os.name == 'nt':  # For Windows
+                os.system(f"start {output_file}")
+            else:
+                st.warning("Unsupported operating system")
+
+            # Provide a download link for the MP3 file
+            st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
+
+            # Convert the translated text to a DOCX document
+            docx_output_file = "translated_text.docx"
+            convert_text_to_word_doc(translated_text, docx_output_file)
+
+            # Provide a download link for the DOCX document
+            st.markdown(get_binary_file_downloader_html("Download DOCX Document", docx_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
