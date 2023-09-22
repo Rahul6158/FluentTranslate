c3c0370cddc9724fc99338db02a39a28e9df681a
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-23 02:22:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,10 @@
 import streamlit as st
 import page1
 import page2
-import page4
 
-# Define a custom sidebar method with styled radio buttons
+# Define a custom sidebar method
 def custom_sidebar():
     st.sidebar.title("Available Options")  # Add a sidebar title
-
-    # Use Streamlit's CSS classes to style the radio buttons
-    st.markdown("""
-    <style>
-    .radio-button-group .css-17qkwz2 label span {
-        font-size: 18px !important;
-    }
-    .radio-button-group .css-17qkwz2 label span:before {
-        width: 18px !important;
-        height: 18px !important;
-        border-radius: 50% !important;
-        top: 50% !important;
-        transform: translateY(-50%) !important;
-    }
-    .radio-button-group .css-17qkwz2 label input:checked ~ span:before {
-        background-color: #1f77b4 !important;
-        border: 2px solid #1f77b4 !important;
-    }
-    .radio-button-group .css-17qkwz2 label {
-        margin-bottom: 10px !important;
-    }
-    </style>
-    """, unsafe_allow_html=True)
 
     # Create radio button group
     page_choice = st.sidebar.radio("", ["Document and Pdf Translation", "Text Translation", "Text Summarization"])
