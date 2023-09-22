0660a88e2eab14895919204d5d67bddef7fb6b92
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-23 02:12:22 +0530

    Update page2.py

--- page2.py
+++ page2.py
@@ -113,7 +113,7 @@ def count_words(text):
     return len(words)
 
 def main():
-    st.image("jangirii.png", width=200)
+    st.image("jangirii.png", width=300)
     st.title("Text Translation and Conversion to Speech (MultiLingual)")
     
     # Get user input
