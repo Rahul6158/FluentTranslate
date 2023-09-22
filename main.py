dbdd909cbba5e13e6a530036a14e73c4ec405dc8
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-23 02:21:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import page1
 import page2
 import page4
 
-# Define a custom sidebar methd with styled radio buttons
+# Define a custom sidebar method with styled radio buttons
 def custom_sidebar():
     st.sidebar.title("Available Options")  # Add a sidebar title
 
-    # Increase font size and style radio buttons
-    st.sidebar.markdown("""
+    # Use Streamlit's CSS classes to style the radio buttons
+    st.markdown("""
     <style>
-    .css-1v3fvcr label span {
+    .radio-button-group .css-17qkwz2 label span {
         font-size: 18px !important;
     }
-    .css-1v3fvcr label span:before {
+    .radio-button-group .css-17qkwz2 label span:before {
         width: 18px !important;
         height: 18px !important;
         border-radius: 50% !important;
         top: 50% !important;
         transform: translateY(-50%) !important;
     }
-    .css-1v3fvcr label input:checked ~ span:before {
+    .radio-button-group .css-17qkwz2 label input:checked ~ span:before {
         background-color: #1f77b4 !important;
         border: 2px solid #1f77b4 !important;
     }
-    .css-1v3fvcr label {
+    .radio-button-group .css-17qkwz2 label {
         margin-bottom: 10px !important;
     }
     </style>
@@ -44,4 +44,3 @@ if page_choice == "Text Translation":
     page2.main() 
 if page_choice == "Text Summarization":
     page4.main()
-
