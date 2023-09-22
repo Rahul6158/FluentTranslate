c4eeeb198da12df4de6edae3724b795a6ae44148
Rahul6158 <128017087+Rahul6158@users.noreply.github.com>
2023-09-23 02:36:47 +0530

    Update page3.py

--- page3.py
+++ page3.py
@@ -1,171 +1,81 @@
 import streamlit as st
-from googletrans import Translator as GoogleTranslator
-from gtts import gTTS
-import os
-import base64
-from docx import Document
+import nltk
+import numpy as np
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import networkx as nx
 
-language_mapping = {
-    "en": "English",
-    "hi": "Hindi",
-    "gu": "Gujarati",
-    "mr": "Marathi",
-    "ta": "Tamil",
-    "te": "Telugu",
-    "ur": "Urdu",
-    "bn": "Bengali",
-    "es": "Spanish",
-    "fr": "French",
-    "de": "German",
-    "it": "Italian",
-    "pt": "Portuguese",
-    "nl": "Dutch",
-    "ja": "Japanese",
-    "ko": "Korean",
-    "ru": "Russian",
-    "ar": "Arabic",
-    "th": "Thai",
-    "tr": "Turkish",
-    "pl": "Polish",
-    "cs": "Czech",
-    "sv": "Swedish",
-    "da": "Danish",
-    "fi": "Finnish",
-    "el": "Greek",
-    "hu": "Hungarian",
-    "uk": "Ukrainian",
-    "no": "Norwegian",
-    "id": "Indonesian",
-    "vi": "Vietnamese",
-    "ro": "Romanian",
-    "fa": "Persian",
-    "iw": "Hebrew",
-    "bg": "Bulgarian",
-    "ca": "Catalan",
-    "hr": "Croatian",
-    "sr": "Serbian",
-    "sk": "Slovak",
-    "sl": "Slovenian",
-    "lt": "Lithuanian",
-    "lv": "Latvian",
-    "et": "Estonian",
-    "is": "Icelandic",
-    "ga": "Irish",
-    "sq": "Albanian",
-    "mk": "Macedonian",
-    "hy": "Armenian",
-    "ka": "Georgian",
-    "ne": "Nepali",
-    "si": "Sinhala",
-    "km": "Khmer",
-    "jw": "Javanese"
-}
+# Download NLTK data (if not already downloaded)
+nltk.download("punkt")
+nltk.download("stopwords")
 
-# Function to translate text using Google Translate
-def translate_text_with_google(text, target_language):
-    google_translator = GoogleTranslator()
+def read_text(text):
+    # Tokenize the text into sentences
+    sentences = nltk.sent_tokenize(text)
 
-    max_chunk_length = 500
-    translated_text = ""
+    # Tokenize the sentences into words and remove stopwords
+    stop_words = set(stopwords.words("english"))
+    words = [nltk.word_tokenize(sentence.lower()) for sentence in sentences]
+    words = [[word for word in sentence if word.isalnum() and word not in stop_words] for sentence in words]
 
-    for i in range(0, len(text), max_chunk_length):
-        chunk = text[i:i + max_chunk_length]
-        translated_chunk = google_translator.translate(chunk, dest=target_language).text
-        translated_text += translated_chunk
+    return sentences, words
 
-    return translated_text
+def sentence_similarity(sentence1, sentence2):
+    # Compute cosine similarity between two sentences
+    vector1 = [0] * len(word_frequencies)
+    vector2 = [0] * len(word_frequencies)
 
-# Function to convert text to speech and save as an MP3 file
-def convert_text_to_speech(text, output_file, language='en'):
-    if text:
-        supported_languages = list(language_mapping.keys())  # Add more supported languages as needed
-        if language not in supported_languages:
-            st.warning(f"Unsupported language: {language}")
-            return
+    for word in sentence1:
+        if word in word_frequencies:
+            vector1[word_frequencies.index(word)] += 1
 
-        tts = gTTS(text=text, lang=language)
-        tts.save(output_file)
+    for word in sentence2:
+        if word in word_frequencies:
+            vector2[word_frequencies.index(word)] += 1
 
-# Function to generate a download link for a file
-def get_binary_file_downloader_html(link_text, file_path, file_format):
-    with open(file_path, 'rb') as f:
-        file_data = f.read()
-    b64_file = base64.b64encode(file_data).decode()
-    download_link = f'<a href="data:{file_format};base64,{b64_file}" download="{os.path.basename(file_path)}">{link_text}</a>'
-    return download_link
+    return 1 - cosine_distance(vector1, vector2)
 
-# Function to convert translated text to a Word document
-def convert_text_to_word_doc(text, output_file):
-    doc = Document()
-    doc.add_paragraph(text)
-    doc.save(output_file)
+def build_similarity_matrix(sentences, words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
 
-# Function to translate text with fallback to Google Translate on error
-def translate_text_with_fallback(text, target_language):
-    try:
-        return translate_text_with_google(text, target_language)
-    except Exception as e:
-        st.warning(f"Google Translate error: {str(e)}")
+    # Fill in the similarity matrix
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            if i != j:
+                similarity_matrix[i][j] = sentence_similarity(words[i], words[j])
 
-# Function to count words in the text
-def count_words(text):
-    words = text.split()
-    return len(words)
+    return similarity_matrix
+
+def generate_summary(text, num_sentences=3):
+    sentences, words = read_text(text)
+    global word_frequencies
+    word_frequencies = [word for sentence in words for word in sentence]
+
+    similarity_matrix = build_similarity_matrix(sentences, words)
+
+    # Use NetworkX to rank sentences based on PageRank algorithm
+    nx_graph = nx.from_numpy_array(similarity_matrix)
+    scores = nx.pagerank(nx_graph)
+
+    # Sort sentences by their scores and get the top N sentences
+    ranked_sentences = sorted(((scores[i], sentence) for i, sentence in enumerate(sentences)), reverse=True)
+    top_sentences = [sentence for _, sentence in ranked_sentences[:num_sentences]]
+
+    return " ".join(top_sentences)
 
 def main():
-    st.image("jangirii.png", width=300)
-    st.title("Text Translation and Conversion to Speech (MultiLingual)")
-    
-    # Get user input
-    text = st.text_area("Enter text to translate and convert to speech:", height=300)
+    st.title("Text Summarization")
 
-    # Show word count as soon as the text is entered
-    word_count = count_words(text)
-    st.subheader(f"Word Count: {word_count} words")
+    text = st.text_area("Enter your text here:", height=400)
 
-    target_language = st.selectbox("Select target language:", list(language_mapping.values()))
-
-    # Add a button to trigger the translation and text-to-speech conversion
-    if st.button("Translate - Convert to Speech and get Translated document"):
-        # Define target_language_code within this scope
-        target_language_code = [code for code, lang in language_mapping.items() if lang == target_language][0]
-
-        # Translate text using Google Translate with error handling
-        translated_text = translate_text_with_fallback(text, target_language_code)
-
-        # Display translated text
-        if translated_text:
-            st.subheader(f"Translated text ({target_language}):")
-            st.write(translated_text)
+    if st.button("Summarize"):
+        if text:
+            summary = generate_summary(text)
+            st.subheader("Summary:")
+            st.write(summary)
         else:
-            st.warning("Translation result is empty. Please check your input text.")
-
-        if translated_text:
-            # Convert translated text to speech
-            output_file = "translated_speech.mp3"
-            convert_text_to_speech(translated_text, output_file, language=target_language_code)
-
-            # Play the generated speech
-            audio_file = open(output_file, 'rb')
-            st.audio(audio_file.read(), format='audio/mp3')
-
-            # Play the generated speech (platform-dependent)
-            if os.name == 'posix':  # For Unix/Linux
-                os.system(f"xdg-open {output_file}")
-            elif os.name == 'nt':  # For Windows
-                os.system(f"start {output_file}")
-            else:
-                st.warning("Unsupported operating system")
-
-            # Provide a download link for the MP3 file
-            st.markdown(get_binary_file_downloader_html("Download Audio File", output_file, 'audio/mp3'), unsafe_allow_html=True)
-
-            # Convert the translated text to a DOCX document
-            docx_output_file = "translated_text.docx"
-            convert_text_to_word_doc(translated_text, docx_output_file)
-
-            # Provide a download link for the DOCX document
-            st.markdown(get_binary_file_downloader_html("Download DOCX Document", docx_output_file, 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'), unsafe_allow_html=True)
+            st.warning("Please enter some text for summarization.")
 
 if __name__ == "__main__":
     main()
